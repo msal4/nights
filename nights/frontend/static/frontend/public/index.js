63d7f29d5c94e0162b7ed381aab9ebda7ec446cc
Mohammed Salman <ms@dijlh.com>
2020-04-21 14:43:29 +0300

    add media models

--- nights/frontend/static/frontend/public/index.js
+++ nights/frontend/static/frontend/public/index.js
@@ -53919,7 +53919,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53576" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51889" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
