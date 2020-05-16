7c02eb8244462c95cedc4f886e172dbda75ad764
Mohammed Salman <msal4@outlook.com>
2020-05-16 15:27:50 +0300

    add redis

--- nights/settings.py
+++ nights/settings.py
@@ -92,6 +92,17 @@ DATABASES = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "nights"
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
