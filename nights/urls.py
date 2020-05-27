71aa7f759e52e72f7f9bd8e1ccbad6f0665087cf
Mohammed Salman <msal4@outlook.com>
2020-05-27 19:58:56 +0300

    remove frontend from urls.py

--- nights/urls.py
+++ nights/urls.py
@@ -8,8 +8,8 @@ from frontend_new import views
 urlpatterns = [
     path('api/', include('api.urls')),
     # TODO: remove from here
-    url(r'^', views.FrontendAppView.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += url(r'^(?:.*)/?', include('frontend.urls')),
+# urlpatterns += url(r'^(?:.*)/?', include('frontend.urls')),
+urlpatterns += url(r'^', views.FrontendAppView.as_view()),
